9da5bd87e7375ddaab1bcfd63b3a1d55d30f0ace
Hekzxx <jesusariasguillen@gmail.com>
2024-06-03 16:14:03 -0500

    listar de country

--- src/app/components/country/country.component.ts
+++ src/app/components/country/country.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ListarComponent } from './listar/listar.component';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-country',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, ListarComponent, MatTableModule],
   templateUrl: './country.component.html',
   styleUrl: './country.component.css'
 })
-export class CountryComponent {
-
+export class CountryComponent implements OnInit {
+  constructor(public route: ActivatedRoute){}
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/components/country/listar/listar.component.ts
+++ src/app/components/country/listar/listar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Country } from '../../../models/Country';
+import { CountryService } from '../../../services/country.service';
 
 @Component({
   selector: 'app-listar',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatPaginatorModule
+  ],
   templateUrl: './listar.component.html',
   styleUrl: './listar.component.css'
 })
-export class ListarComponent {
-
+export class ListarComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'namecountry'];
+  dataSource: MatTableDataSource<Country> = new MatTableDataSource();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  constructor(private cS: CountryService) { }
+  ngOnInit(): void {
+    this.cS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator=this.paginator;
+    })
+  }
 }
