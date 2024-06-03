f1c6e70b6f5374506e510d03c5e3472bcd727fa8
Hekzxx <jesusariasguillen@gmail.com>
2024-06-03 14:32:42 -0500

    Creacion de componentes
    creacion de cada uno de los componentes de cada models

---
+++ src/app/components/ligue/ligue.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ligue',
+  standalone: true,
+  imports: [],
+  templateUrl: './ligue.component.html',
+  styleUrl: './ligue.component.css'
+})
+export class LigueComponent {
+
+}
---
+++ src/app/components/team-xmatch/team-xmatch.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-xmatch',
+  standalone: true,
+  imports: [],
+  templateUrl: './team-xmatch.component.html',
+  styleUrl: './team-xmatch.component.css'
+})
+export class TeamXMatchComponent {
+
+}
