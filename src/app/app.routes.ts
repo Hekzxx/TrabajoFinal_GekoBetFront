4a3212d6c9017ed203e1f8f583d39f2f0529a595
Hekzxx <jesusariasguillen@gmail.com>
2024-06-03 15:33:34 -0500

    routes de las independientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { CountryComponent } from './components/country/country.component';
+import { MatchComponent } from './components/match/match.component';
+import { UserComponent } from './components/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'listarcountry', component:CountryComponent,
+    },
+    {
+        path:'listarmatch', component:MatchComponent,
+    },
+    {
+        path:'listaruser',component:UserComponent,
+    }
+];
