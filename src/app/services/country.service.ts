8a7afc95d1846112f9b6ce315e9272a96b7f88a1
Hekzxx <jesusariasguillen@gmail.com>
2024-06-03 15:53:49 -0500

    Service.ts de Country listar

--- src/app/services/country.service.ts
+++ src/app/services/country.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environements';
+import { Country } from '../models/Country';
 
+const base_url=environment.base;
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
-
-  constructor() { }
+  private url=`${base_url}/countries`
+  constructor(private http: HttpClient) { }
+  list(){
+    return this.http.get<Country[]>(this.url);
+  }
 }
