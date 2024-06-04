a2f5a3bf19610ce96558d0e67f495cc1a16d839b
ego4men <138946891+ego4men@users.noreply.github.com>
2024-06-03 21:38:07 -0500

    Listar Match css, html y ts

--- src/app/components/match/listar/listar.component.ts
+++ src/app/components/match/listar/listar.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Match } from '../../../models/Match';
+import { MatchService } from '../../../services/match.service';
 
 @Component({
   selector: 'app-listar',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule,
+    MatPaginatorModule],
   templateUrl: './listar.component.html',
   styleUrl: './listar.component.css'
 })
-export class ListarComponent {
-
+export class ListarComponent implements OnInit{
+  displayedColumns: string[] = ['id', 'dateMatch', 'versus'];
+  dataSource: MatTableDataSource<Match> = new MatTableDataSource();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  constructor(private mS: MatchService) { }
+  ngOnInit(): void {
+    this.mS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator=this.paginator;
+    })
+  }
 }
