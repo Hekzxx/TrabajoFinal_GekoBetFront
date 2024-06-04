373ba23cd6a74ea0e76392a2cae877b224f3c218
CHANDOtheBOSS <alessandro262003@gmail.com>
2024-06-03 21:06:39 -0500

    ComponentUser

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ListarComponent } from './listar/listar.component';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, ListarComponent, MatTableModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit{
+  constructor(public route: ActivatedRoute){}
+  ngOnInit(): void {
 
+  }
 }
