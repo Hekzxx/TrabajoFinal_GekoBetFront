43783fe5d60b4ca2f53346f2b66505880b7a1137
ego4men <138946891+ego4men@users.noreply.github.com>
2024-06-03 23:16:31 -0500

    Implementacion Match service

--- src/app/services/match.service.ts
+++ src/app/services/match.service.ts
@@ -2,14 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environements';
 import { Match } from '../models/Match';
+import { Subject } from 'rxjs';
 const base_url=environment.base;
 @Injectable({
   providedIn: 'root'
 })
 export class MatchService {
   private url=`${base_url}/Matches`
+  private listacambio = new Subject<Match[]>();
   constructor(private http: HttpClient) { }
   list(){
     return this.http.get<Match[]>(this.url);
   }
+  insert(ma: Match) {
+    return this.http.post(this.url, ma);
+  }
+  setList(listaNueva: Match[]) {
+    this.listacambio.next(listaNueva);
+  }
+  getList() {
+    return this.listacambio.asObservable()
+  }
+
 }
