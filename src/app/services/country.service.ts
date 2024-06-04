eb04760549d33c26afa0465a5164dfb047bde527
Hekzxx <jesusariasguillen@gmail.com>
2024-06-03 22:47:18 -0500

    country.service.ts insertar

--- src/app/services/country.service.ts
+++ src/app/services/country.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environements';
 import { Country } from '../models/Country';
+import { Subject } from 'rxjs';
 
 const base_url=environment.base;
 @Injectable({
@@ -9,8 +10,18 @@ const base_url=environment.base;
 })
 export class CountryService {
   private url=`${base_url}/countries`
+  private listacambio = new Subject<Country[]>();
   constructor(private http: HttpClient) { }
   list(){
     return this.http.get<Country[]>(this.url);
+  }
+  insert(co: Country) {
+    return this.http.post(this.url, co);
+  }
+  setList(listaNueva: Country[]) {
+    this.listacambio.next(listaNueva);
+  }
+  getList() {
+    return this.listacambio.asObservable()
   }
 }
