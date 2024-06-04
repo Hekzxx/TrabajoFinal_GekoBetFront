62da9723de845be254ef88a383a23bd6d8ceba4d
ego4men <138946891+ego4men@users.noreply.github.com>
2024-06-03 21:29:18 -0500

    Implementacion Match Service

--- src/app/services/match.service.ts
+++ src/app/services/match.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../environments/environements';
+import { Match } from '../models/Match';
+const base_url=environment.base;
 @Injectable({
   providedIn: 'root'
 })
 export class MatchService {
-
-  constructor() { }
+  private url=`${base_url}/Matches`
+  constructor(private http: HttpClient) { }
+  list(){
+    return this.http.get<Match[]>(this.url);
+  }
 }
