9b17dc969b9bac0dbe9f05c4ff789a6394bcf9bb
Hekzxx <jesusariasguillen@gmail.com>
2024-06-03 22:44:34 -0500

    app.component.html agregar insertar independientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,33 @@ import { Routes } from '@angular/router';
 import { CountryComponent } from './components/country/country.component';
 import { MatchComponent } from './components/match/match.component';
 import { UserComponent } from './components/user/user.component';
+import { InsertarcountryComponent } from './components/country/insertarcountry/insertarcountry.component';
+import { InsertarmatchComponent } from './components/match/insertarmatch/insertarmatch.component';
+import { InsertaruserComponent } from './components/user/insertaruser/insertaruser.component';
 
 export const routes: Routes = [
     {
         path:'listarcountry', component:CountryComponent,
+        children:[
+            {
+                path:'insertarcountry', component:InsertarcountryComponent,
+            }
+        ]
     },
     {
         path:'listarmatch', component:MatchComponent,
+        children:[
+            {
+                path:'insertarmatch', component:InsertarmatchComponent,
+            }
+        ]
     },
     {
         path:'listaruser',component:UserComponent,
+        children:[
+            {
+                path:'insertaruser', component:InsertaruserComponent,
+            }
+        ]
     }
 ];
