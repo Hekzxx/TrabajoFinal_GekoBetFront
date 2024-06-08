aa95f01bf5fb4684c811c186edad310b5cea9d71
Hekzxx <jesusariasguillen@gmail.com>
2024-06-08 13:45:12 -0500

    season Listar, editar, borrar (css,html,ts)

--- src/app/components/season/listar/listar.component.ts
+++ src/app/components/season/listar/listar.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { RouterLink } from '@angular/router';
+import { Season } from '../../../models/Season';
+import { SeasonService } from '../../../services/season.service';
 
 @Component({
   selector: 'app-listar',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    RouterLink,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './listar.component.html',
   styleUrl: './listar.component.css'
 })
-export class ListarComponent {
+export class ListarComponent implements OnInit{
+  dataSource: MatTableDataSource<Season> = new MatTableDataSource();
+  displayedColumns: string[] = ['paiscountry', 'year', 'accion01', 'accion02'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private sS: SeasonService) { }
+  ngOnInit(): void {
+    this.sS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+    this.sS.getList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+  eliminar(id: number) {
+    this.sS.eliminar(id).subscribe((data) => {
+      this.sS.list().subscribe((data) => {
+        this.sS.setList(data);
+      });
+    });
+  }
 
 }
