e98b160627798cbb00d9e62cb3d784f42625f2d8
Hekzxx <jesusariasguillen@gmail.com>
2024-06-08 12:54:58 -0500

    Season service.ts CRUD

--- src/app/services/season.service.ts
+++ src/app/services/season.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
-
+import { Subject } from 'rxjs';
+import { Season } from '../models/Season';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environements';
+const base_url = environment.base;
 @Injectable({
   providedIn: 'root'
 })
 export class SeasonService {
+  private url =`${base_url}/seasons`;
+  private listacambio = new Subject<Season[]>();
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+  list() {
+    return this.httpClient.get<Season[]>(this.url);
+  }
+  insert(se: Season) {
+    return this.httpClient.post(this.url, se);
+  }
+  setList(listaNueva: Season[]) {
+    this.listacambio.next(listaNueva);
+  }
+  getList() {
+    return this.listacambio.asObservable();
+  }
+  listId(id:number) {
+    return this.httpClient.get<Season>(`${this.url}/${id}`);
+  }
+
+  update(se:Season) {
+    return this.httpClient.put(this.url, se);
+  }
+
+  eliminar(id: number) {
+    return this.httpClient.delete(`${this.url}/${id}`);
+  }
 }
