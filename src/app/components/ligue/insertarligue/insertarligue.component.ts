7b371d1568264e029535ca3382c653c2d90b3557
Hekzxx <jesusariasguillen@gmail.com>
2024-06-08 16:33:46 -0500

    Registrar, Editar, Eliminar ligue

--- src/app/components/ligue/insertarligue/insertarligue.component.ts
+++ src/app/components/ligue/insertarligue/insertarligue.component.ts
@@ -1,12 +1,96 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ActivatedRoute, Params, Router, RouterLink } from '@angular/router';
+import { Ligue } from '../../../models/Ligue';
+import { Country } from '../../../models/Country';
+import { Season } from '../../../models/Season';
+import { SeasonService } from '../../../services/season.service';
+import { CountryService } from '../../../services/country.service';
+import { LigueService } from '../../../services/ligue.service';
 
 @Component({
   selector: 'app-insertarligue',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    CommonModule,
+    NgIf,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    RouterLink,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
   templateUrl: './insertarligue.component.html',
   styleUrl: './insertarligue.component.css'
 })
-export class InsertarligueComponent {
+export class InsertarligueComponent implements OnInit{
+  form: FormGroup = new FormGroup({});
+  ligue: Ligue = new Ligue();
+  listaCountries: Country[] = [];
+  listaSeasons: Season[] = [];
 
+  edicion: boolean = false;
+  id: number = 0;
+
+  constructor(
+    private formBuilder : FormBuilder,
+    private sS: SeasonService,
+    private cS: CountryService,
+    private lS: LigueService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id'];
+      this.edicion = data['id'] != null;
+      this.init();
+    });
+    this.form = this.formBuilder.group({
+      codigo: [''],
+      paiscountry: ['', Validators.required],
+      year: ['', Validators.required],
+      nameligue: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]],
+    });
+    this.sS.list().subscribe((data) => {
+      this.listaSeasons = data;
+    });
+  }
+  registrar(): void {
+    if (this.form.valid) {
+      this.ligue.id= this.form.value.codigo;
+      this.ligue.season.country.id = this.form.value.paiscountry;
+      this.ligue.season.id = this.form.value.year;
+      this.ligue.nameligue = this.form.value.nameligue;
+      this.lS.insert(this.ligue).subscribe((data) => {
+        this.lS.list().subscribe((data) => {
+          this.lS.setList(data);
+        });
+      });
+      this.router.navigate(['listarligue']);
+    }
+  }
+  init(){
+    if (this.edicion) {
+      this.lS.listId(this.id).subscribe((data)=>{
+        this.form = new FormGroup({
+          codigo: new FormControl(data.id),
+          paiscountry: new FormControl(data.season.country.id),
+          year: new FormControl(data.season.id),
+          nameligue: new FormControl(data.nameligue),
+        });
+      });
+    }
+  }
 }
