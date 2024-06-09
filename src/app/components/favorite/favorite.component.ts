e03beb7c49ed0888e2320fe4f01a1775197dad7b
AdrianBenavente <106643726+AdrianBenavente@users.noreply.github.com>
2024-06-09 15:17:52 -0500

    Favorite.html y Favorite.ts

--- src/app/components/favorite/favorite.component.ts
+++ src/app/components/favorite/favorite.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ListarComponent } from './listar/listar.component';
 
 @Component({
   selector: 'app-favorite',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, ListarComponent],
   templateUrl: './favorite.component.html',
   styleUrl: './favorite.component.css'
 })
-export class FavoriteComponent {
-
+export class FavoriteComponent implements OnInit{
+  constructor(public route: ActivatedRoute) {}
+  ngOnInit(): void {}
 }
