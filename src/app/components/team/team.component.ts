d5256d5955cffff740a335afbc0f658056ae2c57
AdrianBenavente <106643726+AdrianBenavente@users.noreply.github.com>
2024-06-09 12:44:28 -0500

    Team.ts y Team.html

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ListarComponent } from './listar/listar.component';
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, ListarComponent],
   templateUrl: './team.component.html',
   styleUrl: './team.component.css'
 })
-export class TeamComponent {
-
+export class TeamComponent implements OnInit{
+constructor(public route: ActivatedRoute){}
+  ngOnInit(): void {}
 }
