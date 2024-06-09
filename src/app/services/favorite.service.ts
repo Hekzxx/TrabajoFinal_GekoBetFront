8a782e8ae96aca1f500bd82d054aa309d2c4b42a
AdrianBenavente <106643726+AdrianBenavente@users.noreply.github.com>
2024-06-09 12:31:33 -0500

    Agregar Servicios de Favorite y Team

--- src/app/services/favorite.service.ts
+++ src/app/services/favorite.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../environments/environements';
+import { Favorite } from '../models/Favorite';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+const base_url = environment.base;
 @Injectable({
   providedIn: 'root'
 })
 export class FavoriteService {
+  private url =`${base_url}/favorities`;
+  private listacambio = new Subject<Favorite[]>();
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  list() {
+    return this.httpClient.get<Favorite[]>(this.url);
+  }
+  insert(fe: Favorite) {
+    return this.httpClient.post(this.url, fe);
+  }
+  setList(listaNueva: Favorite[]) {
+    this.listacambio.next(listaNueva);
+  }
+  getList() {
+    return this.listacambio.asObservable();
+  }
+  listId(id:number) {
+    return this.httpClient.get<Favorite>(`${this.url}/${id}`);
+  }
+
+  update(fe:Favorite) {
+    return this.httpClient.put(this.url, fe);
+  }
+
+  eliminar(id: number) {
+    return this.httpClient.delete(`${this.url}/${id}`);
+  }
 }
--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../environments/environements';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { Team } from '../models/Team';
+const base_url = environment.base;
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
+  private url =`${base_url}/teams`;
+  private listacambio = new Subject<Team[]>();
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  list() {
+    return this.httpClient.get<Team[]>(this.url);
+  }
+  insert(te: Team) {
+    return this.httpClient.post(this.url, te);
+  }
+  setList(listaNueva: Team[]) {
+    this.listacambio.next(listaNueva);
+  }
+  getList() {
+    return this.listacambio.asObservable();
+  }
+  listId(id:number) {
+    return this.httpClient.get<Team>(`${this.url}/${id}`);
+  }
+
+  update(te:Team) {
+    return this.httpClient.put(this.url, te);
+  }
+
+  eliminar(id: number) {
+    return this.httpClient.delete(`${this.url}/${id}`);
+  }
 }
