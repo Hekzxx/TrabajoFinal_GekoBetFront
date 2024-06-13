f71639161bd325167596b871c75f4d277e010a72
Hekzxx <jesusariasguillen@gmail.com>
2024-06-13 03:28:38 -0500

    CRUD TEAMSXMATCHES

--- src/app/components/team-xmatch/team-xmatch.component.ts
+++ src/app/components/team-xmatch/team-xmatch.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ListarComponent } from './listar/listar.component';
 
 @Component({
   selector: 'app-team-xmatch',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, ListarComponent],
   templateUrl: './team-xmatch.component.html',
   styleUrl: './team-xmatch.component.css'
 })
-export class TeamXMatchComponent {
-
+export class TeamXMatchComponent implements OnInit{
+  constructor(public route: ActivatedRoute){}
+  ngOnInit(): void {}
 }
