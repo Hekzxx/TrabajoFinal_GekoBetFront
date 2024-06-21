2248ab0616eaba453faa85c798781fc9e659dfff
Hekzxx <jesusariasguillen@gmail.com>
2024-06-21 01:31:16 -0500

    arreglo tonto

--- src/app/components/reportes/reportes.component.ts
+++ src/app/components/reportes/reportes.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { Reporte01AdrianComponent } from './reporte01-adrian/reporte01-adrian.component';
+import { Reporte01AriasComponent } from './reporte01-arias/reporte01-arias.component';
 
 @Component({
   selector: 'app-reportes',
   standalone: true,
-  imports: [RouterOutlet, Reporte01AdrianComponent],
+  imports: [RouterOutlet, Reporte01AriasComponent],
   templateUrl: './reportes.component.html',
   styleUrl: './reportes.component.css'
 })
