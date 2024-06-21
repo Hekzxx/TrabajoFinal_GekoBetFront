bd9857faec501ce43dbe95ec219a1150b014318e
Hekzxx <jesusariasguillen@gmail.com>
2024-06-21 00:22:52 -0500

    Route Reporte01Arias

--- src/app/components/reportes/reportes.component.ts
+++ src/app/components/reportes/reportes.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { Reporte01AdrianComponent } from './reporte01-adrian/reporte01-adrian.component';
 
 @Component({
   selector: 'app-reportes',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, Reporte01AdrianComponent],
   templateUrl: './reportes.component.html',
   styleUrl: './reportes.component.css'
 })
-export class ReportesComponent {
-
+export class ReportesComponent implements OnInit {
+  constructor(public route: ActivatedRoute){}
+    ngOnInit(): void {
+    }
 }
