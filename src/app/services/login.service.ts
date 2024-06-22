db0875e0120e5b626c37c98ad1292e091bf01006
Hekzxx <jesusariasguillen@gmail.com>
2024-06-22 15:45:15 -0500

    Despliegue

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -11,7 +11,7 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   login(request: JwtRequest) {
-    return this.http.post('http://localhost:8083/login', request);
+    return this.http.post('https://trabajofinal-gekobet.onrender.com/login', request);
   }
   verificar() {
     let token = sessionStorage.getItem('token');
--- src/environments/environements.ts
+++ src/environments/environements.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
+    production: true,
     base: "https://trabajofinal-gekobet.onrender.com"
 }
